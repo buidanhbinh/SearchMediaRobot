29a336db8cfa248c7f858cb992d55047ca119174
SAF ONE <77989182+AsmSafone@users.noreply.github.com>
2021-06-09 15:06:22 +0600

    Update userbot.py

--- plugins/userbot.py
+++ plugins/userbot.py
@@ -16,12 +16,12 @@ async def index_files(bot, message):
     if not USERBOT_STRING_SESSION:
         await message.reply('Set `USERBOT_STRING_SESSION` in info.py file or in environment variables.')
     elif len(message.command) == 1:
-        await message.reply('Please specify channel username or id in command.\n\n'
+        await message.reply('Please Specify Channel Username or ID In Command.\n\n'
                             'Example: `/index -10012345678`')
     elif lock.locked():
-        await message.reply('Wait until previous process complete.')
+        await message.reply('Wait Until Previous Process Complete!')
     else:
-        msg = await message.reply('Processing...⏳')
+        msg = await message.reply('Processing... 🔄')
         raw_data = message.command[1:]
         user_bot = Client(USERBOT_STRING_SESSION, API_ID, API_HASH)
         chats = [int(chat) if id_pattern.search(chat) else chat for chat in raw_data]
@@ -61,4 +61,4 @@ async def index_files(bot, message):
                 logger.exception(e)
                 await msg.edit(f'Error: {e}')
             else:
-                await msg.edit(f'Total {total_files} checked!')
+                await msg.edit(f'Total {total_files} Checked!')

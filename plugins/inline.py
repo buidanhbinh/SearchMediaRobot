0e6255ac420a650e026dde34d53472f31b9cd605
SAF ONE <77989182+AsmSafone@users.noreply.github.com>
2021-06-09 16:09:54 +0600

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -45,7 +45,7 @@ async def answer(bot, query):
                 title=file.file_name,
                 file_id=file.file_id,
                 caption=file.caption or "",
-                description=f'Size: {get_size(file.file_size)} | Type: {file.file_type}',
+                description=f'Size: {get_size(file.file_size)} | File Type: {file.mime_type}',
                 reply_markup=reply_markup))
 
     if results:
